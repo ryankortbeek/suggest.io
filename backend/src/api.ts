571242342820116ba473e09fc31d3ad90e60150d
Justin Boileau <jboileau@ualberta.ca>
2021-05-08 16:53:39 -0600

    getEvents function changed to use lat, long, and radius

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
 
 interface IEvent {
-    id: number,
+    id: string,
     name: string,
-    image: string,
+    image_url: string,
     description: string
 }
 
@@ -12,9 +12,11 @@ export interface IEventResponse {
     events: Event[];
 }
 
-export function getEvents() {
+// e.g.getEvents("53.53216861500475", "-113.57363822706881", "40000")
+// need to post in api key in auth header like "Bearer <key>"
+export function getEvents(latitude: string, longitude: string, radius: string) {
 
-    const res = axios.get("https://api.yelp.com/v3/events?location=edmonton&limit=50&offset=50", {
+    const res = axios.get(`https://api.yelp.com/v3/events?latitude=${latitude}&longitude=${longitude}&radius=${radius}&limit=50`, {
         headers: {
           "Authorization": "Bearer "
         }
@@ -22,14 +24,12 @@ export function getEvents() {
       .then((response) => {
         const results: IEventResponse = {
           id: new Date().getTime(),
-          events: response.data.events.map(({id, name, image_url, description})=>({id, name, image_url, description}))
+          events: response.data.events.map(({id, name, image_url, description}: IEvent)=>({id, name, image_url, description}))
         };
-        return results;
         console.log(results);
+        return results;
       });
       return res;
   }
 
-export function getMatchedEvents(userId: string) {
-
-}
+export function getMatchedEvents(userId: string) {}
