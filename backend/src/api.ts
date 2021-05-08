f136587dad57f39b03c9819db9c40941c632e4c0
Justin Boileau <jboileau@ualberta.ca>
2021-05-08 15:54:29 -0600

    get events function

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,23 +1,35 @@
-interface Event {
+import axios, { AxiosResponse } from 'axios';
+
+interface IEvent {
     id: number,
     name: string,
     image: string,
     description: string
 }
 
-export interface EventResponse {
+export interface IEventResponse {
     id: number;
     events: Event[];
 }
 
 function getEvents() {
 
-}
+    const res = axios.get("https://api.yelp.com/v3/events?location=edmonton&limit=50&offset=50", {
+        headers: {
+          "Authorization": "Bearer "
+        }
+      })
+      .then((response) => {
+        const results: IEventResponse = {
+          id: new Date().getTime(),
+          events: response.data.events.map(({id, name, image_url, description})=>({id, name, image_url, description}))
+        };
+        return results;
+        console.log(results);
+      });
+      return res;
+  }
 
 function getMatchedEvents(userId: string) {
-
-}
-
-function postMatch(eventId: string, isMatch: boolean) {
 
 }
