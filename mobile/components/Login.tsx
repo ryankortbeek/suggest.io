ece699ff0bcaeb0372e35baebdcd6640ae7c73a5
Daniel Rojas <drojasca@ualberta.ca>
2021-05-08 16:49:57 -0600

    simple login

--- mobile/components/Login.tsx
+++ mobile/components/Login.tsx
@@ -1,14 +1,16 @@
-import React, { FC } from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RouteProp } from '@react-navigation/native';
-import { RootStackParamList } from '../App';
+import React, { ChangeEvent, FC } from "react";
+import { View, Text } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RouteProp } from "@react-navigation/native";
+import { RootStackParamList } from "../App";
+import { TextInput, Button } from "react-native-paper";
+import { useState, useEffect } from "react";
 
-type LoginScreenRouteProp = RouteProp<RootStackParamList, 'Login'>;
+type LoginScreenRouteProp = RouteProp<RootStackParamList, "Login">;
 
 type LoginScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
-  'Login'
+  "Login"
 >;
 
 type Props = {
@@ -17,9 +19,99 @@ type Props = {
 };
 
 export const Login: FC<Props> = ({ route, navigation }) => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [vPassword, setVPassword] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [isCreate, setIsCreate] = useState<boolean>(false);
+
+  const onChangeUsername = (username: string): void => {
+    setUsername(username);
+  };
+
+  const onChangePassword = (password: string): void => {
+    setPassword(password);
+  };
+
+  const onChangeName = (name: string): void => {
+    setName(name);
+  };
+
+  const onChangeVPassword = (password: string): void => {
+    setVPassword(password);
+  };
+  const changeSignUp = (): void => {
+    setUsername("");
+    setPassword("");
+    setIsCreate(true);
+  };
+
+  const login = () => {
+    return (
+      <>
+        <TextInput
+          label="username"
+          value={username}
+          onChangeText={onChangeUsername}
+        />
+        <TextInput
+          secureTextEntry={true}
+          label="password"
+          value={password}
+          onChangeText={onChangePassword}
+        />
+        <Button
+          onPress={() => navigation.navigate("Main", { userInfo: "Jane" })}
+          mode="outlined"
+        >
+          LOGIN
+        </Button>
+        <Text>
+          Don't have an account?{" "}
+          <Text onPress={() => changeSignUp()} style={{ color: "#000", fontWeight:"bold" }}>
+            Sign up
+          </Text>
+        </Text>
+      </>
+    );
+  };
+
+  const signup = () => {
+    return (
+      <>
+        <TextInput label="Name" value={name} onChangeText={onChangeName}/>
+        <TextInput
+          label="username"
+          value={username}
+          onChangeText={onChangeUsername}
+        />
+        <TextInput
+          secureTextEntry={true}
+          label="password"
+          value={password}
+          onChangeText={onChangePassword}
+        />
+        <TextInput
+          secureTextEntry={true}
+          label="confirm password"
+          value={vPassword}
+          onChangeText={onChangeVPassword}
+          error={vPassword != password}
+        />
+        <Button
+          onPress={() => navigation.navigate("Main", { userInfo: "Jane" })}
+          mode="outlined"
+          disabled={vPassword != password || password == ""}
+        >
+          SIGN UP
+        </Button>
+      </>
+    );
+  };
   return (
     <View>
-      <Text> THIS IS LOGIN </Text>
+      <Text> Suggest.io </Text>
+      {isCreate ? signup() : login()}
     </View>
   );
 };
