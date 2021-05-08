5bb3311bdf460e3e5d8f0221aa17671c649c6616
ryankortbeek <rakortbe@ualberta.ca>
2021-05-08 14:09:34 -0600

    routing

--- backend/src/db_handler.ts
+++ backend/src/db_handler.ts
@@ -1,10 +1,14 @@
 // DB stuff
-const admin = require('firebase-admin');
-
 const serviceAccount = require('../firebase-admin/service-account.json');
+
+const admin = require('firebase-admin');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
+
+export function handleEventResponse(userId: string, eventId: string, match: boolean) {
+
+}
