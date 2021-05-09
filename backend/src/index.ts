96e537c75807953b474e005b83d9c98396a59d01
ryankortbeek <rakortbe@ualberta.ca>
2021-05-08 21:50:43 -0600

    added status number handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,33 +13,57 @@ const fs = require('fs');
 
 app.get('/', (_, res) => {
     console.log('Welcome to suggest.io!');
-    res.json({message: 'Welcome to suggest.io!'});
+    res.status(200).json({message: 'Welcome to suggest.io!'});
 })
 
 app.get('/events/location/:latitude-:longitude-:radius', (req, res) => {
     console.log('GET events');
     console.log(req.params);
-    getEvents(req.params['latitude'], req.params['longitude'], req.params['radius']).then((val) => {
-        res.json(val)
-    })
+    getEvents(req.params['latitude'], req.params['longitude'], req.params['radius'])
+        .then((val) => {
+            console.log(val);
+            res.status(200).json(val);
+        }, (rej) => {
+            console.log(rej);
+            res.status(404).end();
+        })
+        .catch((e) => {
+            console.log(e);
+            res.status(404).end();
+        });
 })
 
 app.get('/matched-events/user/:userId', (req, res) => {
     console.log('GET matched events');
     console.log(req.params);
-    res.json(getMatchedEvents(req.params['userId']));
+    getMatchedEvents(req.params['userId'])
+        .then((val) => {
+            console.log(val);
+            res.status(200).json(val);
+        }, (rej) => {
+            console.log(rej);
+            res.status(404).end();
+        })
+        .catch((e) => {
+            console.log(e);
+            res.status(404).end();
+        });
 })
 
 app.post('/match', (req, res) => {
     console.log('POST match');
     console.log(req.body);
-    handleEventResponse(req.body.userId, req.body.eventId, req.body.isMatch);
+    handleEventResponse(req.body.userId, req.body.eventId, req.body.isMatch)
+        .then((val) => {res.status(200).json(val)}, (rej) => {res.status(406).end()})
+        .catch((e) => {res.status(406).end()});
 })
 
 app.post('/sign-up', (req, res) => {
     console.log('POST sign up');
     console.log(req.body);
-    signUpUser(req.body.userId).then((val)=>{console.log(val)}, (rej)=>{console.log(rej)}).catch((e)=>{console.log(e)});
+    signUpUser(req.body.userId)
+        .then((val) => {res.status(200).json(val)}, (rej) => {res.status(406).end()})
+        .catch((e) => {res.status(406).end()});
 })
 
 // HTTPS stuff
