15336f1702968b742539e116a6d366bc6593f510
Justin Boileau <jboileau@ualberta.ca>
2021-05-08 19:45:00 -0600

    moved yelp api key and added get matched events

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
+import { response } from 'express';
+import { each } from 'jquery';
 
 interface IEvent {
     id: string,
@@ -9,15 +11,17 @@ interface IEvent {
 
 export interface IEventResponse {
     id: number;
-    events: Event[];
+    events: IEvent[];
 }
+
+const YELP_API_KEY = "1ivgB27DrOOF9NbyWW95E1w3eWxw1MD21uhjZaxI1jPXWaEn-m06uNVdvXVecLc9nt663pSNIdiHIyGYi-UMqBiIw0o_BPhTy50GUYhBMmWyfRNXnOYZurdy7LOWYHYx"
 
 // e.g.getEvents("53.53216861500475", "-113.57363822706881", "40000")
 export function getEvents(latitude: string, longitude: string, radius: string) {
 
     const res = axios.get(`https://api.yelp.com/v3/events?latitude=${latitude}&longitude=${longitude}&radius=${radius}&limit=50`, {
         headers: {
-          "Authorization": "Bearer 1ivgB27DrOOF9NbyWW95E1w3eWxw1MD21uhjZaxI1jPXWaEn-m06uNVdvXVecLc9nt663pSNIdiHIyGYi-UMqBiIw0o_BPhTy50GUYhBMmWyfRNXnOYZurdy7LOWYHYx"
+          "Authorization": `Bearer ${YELP_API_KEY}`
         }
       })
       .then((response) => {
@@ -30,4 +34,24 @@ export function getEvents(latitude: string, longitude: string, radius: string) {
       return res;
   }
 
-export function getMatchedEvents(userId: string) {}
+export function getMatchedEvents(userId: string) {
+
+  // Function that gets array of event IDs for a given user ID
+  let eventIds: string[] = ["edmonton-virtual-yelp-event-gourmai-dumplings-with-mai-nguyen", "edmonton-yelps-prohibition-party"].map(id => `https://api.yelp.com/v3/events/${id}`);
+  console.log(eventIds);
+
+  const res = axios.all(eventIds.map(url => axios.get(url, {headers: {"Authorization": `Bearer ${YELP_API_KEY}`}})))
+    .then(responseArr => {
+      const results = responseArr.map(({data})=>({data}));
+      return results;
+    });
+  return res;
+}
+
+// getEvent("edmonton-virtual-yelp-event-gourmai-dumplings-with-mai-nguyen").then((val) => {
+//   console.log(val)
+// })
+
+// getMatchedEvents("3005").then((val) => {
+//   console.log(val);
+// });
