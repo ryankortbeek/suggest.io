691f3aa7ade95d7a42dafedf24fac9fedda060ff
Cyrus Diego <cyrus.d04@gmail.com>
2021-05-09 03:42:51 -0600

    Port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import {getEvents, getMatchedEvents} from './api';
-import {checkAuth, handleEventResponse, signUpUser} from './db_handler';
+import { getEvents, getMatchedEvents } from './api';
+import { checkAuth, handleEventResponse, signUpUser } from './db_handler';
 
 // Routing
 const app = express();
@@ -12,102 +12,130 @@ const fs = require('fs');
 // })
 
 app.get('/', (_, res) => {
-    console.log('Welcome to suggest.io!');
-    res.status(200).json({message: 'Welcome to suggest.io!'});
-})
+  console.log('Welcome to suggest.io!');
+  res.status(200).json({ message: 'Welcome to suggest.io!' });
+});
 
-app.get('/events/user/:userId/location/:latitude-:longitude-:radius', (req, res) => {
+app.get(
+  '/events/user/:userId/location/:latitude-:longitude-:radius',
+  (req, res) => {
     console.log('GET events');
     console.log(req.params);
-    getEvents(req.params['userId'], req.params['latitude'], req.params['longitude'], req.params['radius'])
-        .then((val) => {
-            console.log('No events found for specified parameters...')
-            console.log(val);
-            res.status(200).json(val);
-        }, (rej) => {
-            console.log(rej);
-            res.status(404).end();
-        })
-        .catch((e) => {
-            console.log(e);
-            res.status(404).end();
-        });
-})
+    getEvents(
+      req.params['userId'],
+      req.params['latitude'],
+      req.params['longitude'],
+      req.params['radius']
+    )
+      .then(
+        (val) => {
+          console.log('No events found for specified parameters...');
+          console.log(val);
+          res.status(200).json(val);
+        },
+        (rej) => {
+          console.log(rej);
+          res.status(404).end();
+        }
+      )
+      .catch((e) => {
+        console.log(e);
+        res.status(404).end();
+      });
+  }
+);
 
 app.get('/matched-events/user/:userId', (req, res) => {
-    console.log('GET matched events... ' + req.params);
-    console.log(req.params);
-    getMatchedEvents(req.params['userId'])
-        .then((val) => {
-            if (val == null) {
-                console.log('No matched events for specified user...');
-                res.status(404).end();
-            }
-            res.status(200).json(val);
-        }, (rej) => {
-            console.log(rej);
-            res.status(404).end();
-        })
-        .catch((e) => {
-            console.log(e);
-            res.status(404).end();
-        });
-})
+  console.log('GET matched events... ' + req.params);
+  console.log(req.params);
+  getMatchedEvents(req.params['userId'])
+    .then(
+      (val) => {
+        if (val == null) {
+          console.log('No matched events for specified user...');
+          res.status(404).end();
+        }
+        res.status(200).json(val);
+      },
+      (rej) => {
+        console.log(rej);
+        res.status(404).end();
+      }
+    )
+    .catch((e) => {
+      console.log(e);
+      res.status(404).end();
+    });
+});
 
 app.post('/match', (req, res) => {
-    console.log('POST match');
-    console.log(req.body);
-    if (req.body.length == 4) {
-        handleEventResponse(req.body.userId, req.body.eventId, req.body.isMatch, req.body.eventCategory)
-        .then((val) => {
-            if (val == null) {
-                res.status(404).end();
-            }
-            res.status(200).end();
-        }, (rej) => {
-            console.log(rej);
+  console.log('POST match');
+  console.log(req.body);
+  if (req.body.length == 4) {
+    handleEventResponse(
+      req.body.userId,
+      req.body.eventId,
+      req.body.isMatch,
+      req.body.eventCategory
+    )
+      .then(
+        (val) => {
+          if (val == null) {
             res.status(404).end();
-        })
-        .catch((e) => {
-            console.log(e);
+          }
+          res.status(200).end();
+        },
+        (rej) => {
+          console.log(rej);
+          res.status(404).end();
+        }
+      )
+      .catch((e) => {
+        console.log(e);
+        res.status(404).end();
+      });
+  } else {
+    handleEventResponse(req.body.userId, req.body.eventId, req.body.isMatch)
+      .then(
+        (val) => {
+          if (val == null) {
             res.status(404).end();
-        });
-    } else {
-        handleEventResponse(req.body.userId, req.body.eventId, req.body.isMatch)
-        .then((val) => {
-            if (val == null) {
-                res.status(404).end();
-            }
-            res.status(200).end();
-        }, (rej) => {
-            console.log(rej);
-            res.status(404).end();
-        })
-        .catch((e) => {
-            console.log(e);
-            res.status(404).end();
-        });
-    }
-})
+          }
+          res.status(200).end();
+        },
+        (rej) => {
+          console.log(rej);
+          res.status(404).end();
+        }
+      )
+      .catch((e) => {
+        console.log(e);
+        res.status(404).end();
+      });
+  }
+});
 
 app.post('/sign-up', (req, res) => {
-    console.log('POST sign up');
-    console.log(req.body);
-    signUpUser(req.body.userId)
-        .then((val) => {
-            if (val == null) {
-                res.status(404).end();
-            }
-            res.status(200).end();
-        }, (rej) => {
-            console.log(rej);
-            res.status(404).end();
-        })
-        .catch((e) => {
-            console.log(e);
-            res.status(404).end();
-        });
-})
+  console.log('POST sign up');
+  console.log(req.body);
+  signUpUser(req.body.userId)
+    .then(
+      (val) => {
+        if (val == null) {
+          res.status(404).end();
+        }
+        res.status(200).end();
+      },
+      (rej) => {
+        console.log(rej);
+        res.status(404).end();
+      }
+    )
+    .catch((e) => {
+      console.log(e);
+      res.status(404).end();
+    });
+});
 
 // HTTPS stuff
 // const options = {
@@ -119,6 +147,6 @@ app.post('/sign-up', (req, res) => {
 //     console.log('The application is listening on port 3000!');
 // })
 
-app.listen(3000, () => {
-    console.log('The application is listening on port 3000!');
-})
+app.listen(process.env.PORT, () => {
+  console.log('The application is listening on port 3000!');
+});
